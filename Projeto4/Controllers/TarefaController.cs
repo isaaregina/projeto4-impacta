c7943e8e8687535dc18b20831ae063d20063e2e0
Isabella Regina <isabellaregina344@gmail.com>
2024-05-10 17:52:47 -0300

    Sistema para Lista de Tarefas

---
+++ Projeto4/Controllers/TarefaController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projeto4.Models;
+
+namespace Projeto4.Controllers
+{
+    public class TarefaController : Controller
+    {
+        private static List<Tarefa> _tarefas = new List<Tarefa>
+        {
+            new Tarefa { TarefaId=1, NomeTarefa="Varrer a casa", DataInicio=DateOnly.FromDateTime(DateTime.Now), Status="Por Fazer" },
+            new Tarefa { TarefaId=2, NomeTarefa="Lavar os pratos", DataInicio=DateOnly.FromDateTime(DateTime.Now), Status="Por Fazer" },
+            new Tarefa { TarefaId=3, NomeTarefa="Estudar", DataInicio=DateOnly.FromDateTime(DateTime.Now), Status="Por Fazer" },
+        };
+
+        public IActionResult Index()
+        {
+            return View(_tarefas);
+        }
+
+        [HttpGet] // Anotação de PEGAR
+        public IActionResult Criar() //chama o form de cadastro
+        {
+            return View();
+        }
+
+        [HttpPost] // Anotação de ENVIAR
+        public IActionResult Criar(Tarefa tarefa) //recebe os dados do form
+        {
+            if (ModelState.IsValid)
+            {
+                tarefa.TarefaId = _tarefas.Count > 0 ? _tarefas.Max(c => c.TarefaId) + 1 : 1;
+                _tarefas.Add(tarefa);
+            }
+            //return View(cliente);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Deletar(int id)
+        {
+            var tarefa = _tarefas.FirstOrDefault(c => c.TarefaId == id);
+            if (tarefa == null) return NotFound();
+
+            _tarefas.Remove(tarefa);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Editar(int id)
+        {
+            var tarefa = _tarefas.FirstOrDefault(c => c.TarefaId == id);
+            if (tarefa == null) return NotFound();
+
+            return View(tarefa);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(Tarefa tarefa)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingTarefa = _tarefas.FirstOrDefault(c => c.TarefaId == tarefa.TarefaId);
+                if (existingTarefa != null)
+                {
+                    existingTarefa.NomeTarefa = tarefa.NomeTarefa;
+                    existingTarefa.Status = tarefa.Status;
+
+                    // Define a data de início apenas se o status for "Por Fazer"
+                    existingTarefa.DataInicio = existingTarefa.Status == "Por Fazer" ? DateOnly.FromDateTime(DateTime.Now) : existingTarefa.DataInicio;
+                    existingTarefa.DataConclusao = existingTarefa.Status == "Concluído" ? DateOnly.FromDateTime(DateTime.Now) : existingTarefa.DataConclusao;
+
+                }
+                return RedirectToAction("Index");
+            }
+            return View(tarefa);
+        }
+
+        public IActionResult Detalhes(int id)
+        {
+            var tarefa = _tarefas.FirstOrDefault(c => c.TarefaId == id);
+            if (tarefa == null) return NotFound();
+
+            return View(tarefa);
+        }
+
+        public IActionResult TarefaFazer()
+        {
+            var tarefasPorFazer = _tarefas.Where(t => t.Status == "Por Fazer").ToList();
+            return View(tarefasPorFazer);
+        }
+
+        public IActionResult TarefaConcluida()
+        {
+            var tarefasPorFazer = _tarefas.Where(t => t.Status == "Concluído").ToList();
+            return View(tarefasPorFazer);
+        }
+    }
+}
---
+++ Projeto4/Models/Tarefa.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projeto4.Models
+{
+    public class Tarefa
+    {
+        public int TarefaId { get; set; }
+
+        [Required]
+        //[StringLength(30, MinimumLength = 2, ErrorMessage = "No máximo 30 caracteres permitidos e no mínimo 2.")]
+        [Display(Name = "Nome")]
+        public string? NomeTarefa { get; set; }
+
+        [Display(Name = "Data de Início")]
+        public DateOnly? DataInicio { get; set; }
+
+        [Display(Name = "Data de Conclusão")]
+        public DateOnly? DataConclusao {  get; set; }
+
+        [Required]
+        public string? Status { get; set; }
+    }
+}
